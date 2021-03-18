152ed975353d2a48a827caffde0fba0dbb64a92e
Mathilda Pettersson <mathildapetterssonn@hotmail.com>
2021-03-18 09:05:13 +0100

    started to style portfolio

--- script.js
+++ script.js
@@ -4,47 +4,58 @@ printStartpage();
 
 function printStartpage(id) {
     console.log("skriv ut startsida");
-        main.style.backgroundColor = "#f3edea";
-        main.innerHTML = `
+    main.innerHTML = `
+    <section class="start-container">
         <article>
             <h3>Profil</h3>
             <span class="text-shadow"> </span>
             <p>Jag utbildar mig just nu till frontend-utvecklare efter 10 år på HM, senast som visual merchandiser. Jag kastar mig in i en ny branch nu och är hungrig på ny kunskap inom frontend, programering och nya arbetssätt! Jag gillar design, färg och form och har intresse för inredning på fritiden.</p>
         </article>
-        <article>
-            <h3>Github</h3>
-            <span class="text-shadow"> </span>
-            <h4>Repos</h4>
-            <p id="repo1"></p>
-        </article>`
-
-        const repo1 = document.getElementById("repo1");
-        fetch("https://api.github.com/users/Mathildap/repos")
-        .then(response => response.json())
-        .then(data => {
-        // console.log(data[1]);
-
-        repo1.insertAdjacentHTML("beforeend", "<i class='fab fa-github gh'></i>" + "<a target='_blank' class='github' href='"+ data[1].html_url +"'>" + "To-do List" + "</a>");
-        });
+       
+    </section>`
 };
 
 function printPortfolio() {
     console.log("skriv ut portfolio");
-    main.style.backgroundColor = "#f3edea";
     main.innerHTML = `
+    <section class="portfolio-container">
     <article>
         <h3>Portfolio</h3>
         <span class="text-shadow"> </span>
         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque, deserunt odit sapiente, cum possimus itaque quod explicabo nisi excepturi animi qui sunt obcaecati nemo dolore eligendi veritatis repellendus consequuntur? Dignissimos!
         <p>
+        <article>
+        <h3>Github</h3>
+        <span class="text-shadow"> </span>
+        <h4>Repos</h4>
+        </article>
+
+        <div class="portfolio-github-container">
+            <article class="github-box">
+                <p id="repo1"></p>
+            </article>
+            <article class="github-box">Hej</article>
+            <article class="github-box">Hej</article>
+            <article class="github-box">Hej</article>
+        </div>
     </article>
+    </section>
     `
+    fetch("https://api.github.com/users/Mathildap/repos")
+    .then(response => response.json())
+    .then(data => {
+
+        document.getElementById("repo1").insertAdjacentHTML("beforeend", "<i class='fab fa-github gh'></i>" + "<a target='_blank' class='github' href='"+ data[1].html_url +"'>" + "To-do List" + "</a>");
+
+    });
 };
+
+
 
 function printExperience() {
     console.log("skriv ut erfarenhet");
-    main.style.backgroundColor = "#f3edea";
     main.innerHTML = `
+    <section class="experience-container">
     <article>
     <h3>Utbildning</h3>
     <span class="text-shadow"> </span>
@@ -75,14 +86,14 @@ function printExperience() {
     Johanssons Café. 
     </p>
   </article>
+  </section>
   `
 };
 
 function printContact() {
     console.log("skriv ut kontakt");
-    main.style.backgroundColor = "rgba(255, 255, 255, 0.5)";
-    main.style.border = "1px solid rgba(255,255,255,0.4)"
     main.innerHTML = `
+    <section class="contact-container">
     <article>
         <h3>Kontakt</h3>
         <span class="text-shadow"> </span>
@@ -121,6 +132,7 @@ function printContact() {
         </div>
         </a>
     </article>
+    </section>
     `
 };
 
