7111fae19c3680b84b1a8ae79edba65a7ca5fcde
Mathilda Pettersson <77104124+Mathildap@users.noreply.github.com>
2021-03-22 10:33:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function printStartpage(id) {
             <p>Välkommen till min sida, jag heter Mathilda, är 28år och utbildar mig just nu till frontend-utvecklare. Har senaste 10 åren jobbat på HM, största del i butik, senast som visual merchandiser, men en del resande har jag också hunnit med. Jag kastar mig nu in i en ny branch är än så länge säkert på att det är detta jag jobba med! Jag är hungrig på ny kunskap inom frontend och programering och allt där till. Jag gillar design, färg och form och har intresse för inredning på fritiden.
             <br>
             <br>
-            Våren 2021 kommer jag ge mig ut på LIA och söker just nu en plats, skulle ditt företag vara intresserade så hör gärna av er!</p>
+            Våren 2022 kommer jag ge mig ut på LIA och söker just nu en plats, skulle ditt företag vara intresserade så hör gärna av er!</p>
             </div>
         </article>
        
